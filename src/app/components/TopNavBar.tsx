bf2e1f7bc5c206217c80c1dc3ae71454a66d192d
jonathancaleb <jonathancaleb230@gmail.com>
2023-11-16 16:56:24 +0300

    :3rd_place_medal:

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -23,7 +23,7 @@ export const TopNavBar = () => {
           <Image
             src={logoSrc}
             alt="WorkFolio Logo"
-            className="h-8 w-full"
+            className="height: 12rem w-full"
             priority
           />
         </Link>
