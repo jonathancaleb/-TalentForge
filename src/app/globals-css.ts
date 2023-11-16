45e2b2c111b1dc230af0f86ac581aa9029c77018
jonathancaleb <jonathancaleb230@gmail.com>
2023-11-16 17:07:37 +0300

    :adhesive_bandage:

--- src/app/globals-css.ts
+++ src/app/globals-css.ts
@@ -1,5 +1,5 @@
 export const CSS_VARIABLES = {
-  "--top-nav-bar-height": "3.5rem",
+  "--top-nav-bar-height": "4.0rem",
   "--resume-control-bar-height": "3rem",
   "--resume-padding": "1.5rem",
 } as const;
