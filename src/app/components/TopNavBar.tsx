6681ec77557f47d6dc03b67fec58317aeac1c945
jonathancaleb <jonathancaleb230@gmail.com>
2023-11-16 16:50:14 +0300

    :8ball:

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -23,7 +23,7 @@ export const TopNavBar = () => {
           <Image
             src={logoSrc}
             alt="WorkFolio Logo"
-            className="h-12rem w-full"
+            className="h-6rem w-full"
             priority
           />
         </Link>
