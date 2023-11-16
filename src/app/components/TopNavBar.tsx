d21ca0af611fc7c6b3d316475973db635b569163
jonathancaleb <jonathancaleb230@gmail.com>
2023-11-16 17:58:42 +0300

    :100:

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -2,7 +2,7 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
-import logoSrc from "public/logomin.svg";
+import logoSrc from "public/logominx.svg";
 import { cx } from "lib/cx";
 
 export const TopNavBar = () => {
